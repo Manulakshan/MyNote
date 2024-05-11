e8ad2cd2dc8276f2062ebd43d9146ac2b4bb1008
Manulakshan <manulakshan.y@gmail.com>
2024-05-11 17:37:50 +0530

    update commit

--- app/src/main/java/com/example/mynote/NotesAdapter.kt
+++ app/src/main/java/com/example/mynote/NotesAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.mynote
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView //RecyclerView.Adapter<NotesAdapter.NoteViewHolder>{
 
@@ -12,6 +14,7 @@ class NotesAdapter (private var notes: List<Note>, context: Context) : RecyclerV
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
         val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
+        val updateButton: ImageView = itemView.findViewById(R.id.updateButton)
 
     }
 
@@ -27,7 +30,12 @@ class NotesAdapter (private var notes: List<Note>, context: Context) : RecyclerV
         val note = notes[position]
         holder.titleTextView.text = note.title
         holder.contentTextView.text = note.content
-
+        holder.updateButton.setOnClickListener{
+            val intent = Intent(holder.itemView.context, UpdateNoteActivity::class.java).apply{
+                putExtra("note_id", note.id)
+            }
+            holder.itemView.context.startActivity(intent)
+        }
     }
 
     fun refreshData(newNotes: List<Note>){
